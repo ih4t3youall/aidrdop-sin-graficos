a80600e19543814cee45457ec0e4c07ddaac201f
developproyectos@gmail.com <developproyectos@gmail.com@efccddb1-b7da-b534-b4b3-ee3e2ba5f1ec>
2013-12-24 03:31:38 +0000

    se elimina swing

--- trunk/Airdrop/src/main/java/ar/com/airdrop/Main.java
+++ trunk/Airdrop/src/main/java/ar/com/airdrop/Main.java
@@ -1,14 +1,10 @@
 package ar.com.airdrop;
 
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
 
  
  
 public class Main {
 	
-	public static JTextField jTextField1 = new JTextField();
-	public static JTextArea jTextArea1 = new JTextArea();
 	
 	
 	public static void main(String[] args) {
